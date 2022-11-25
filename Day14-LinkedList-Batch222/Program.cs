5aeb68467e73ff096751e1e96b3980252d18e723
Aneesh <katta.aneesh597@gmail.com>
2022-11-25 22:50:50 +0530

    added main branch

---
+++ Day14-LinkedList-Batch222/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day14_LinkedList_Batch222
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome! Enter a program number\n1.LinkedListUc1");
+        }
+    }
+}
